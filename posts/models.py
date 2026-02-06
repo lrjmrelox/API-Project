a729100f300695adaeb369d20780b27054fdb05c
lrjmrelox <lr.jmrelox@mmdc.mcl.edu.ph>
2026-02-06 20:37:08 +0800

    saving

--- posts/models.py
+++ posts/models.py
@@ -13,7 +13,7 @@ class User(models.Model):
 
 class Post(models.Model):
     content = models.TextField()
-    author = models.ForeignKey(User, related_name='posts', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
 
